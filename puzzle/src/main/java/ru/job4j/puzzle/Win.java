dd2b6f1b2e64b210b56bd734a2fd6ced7e690d77
AsherKikodze <asher.kikodze@gmail.com>
2021-05-16 19:22:57 +0300

    1.1.7. Контрольные вопросы и тестовое задание.  2. Сокобан. Тестовое задание.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,38 @@
 package ru.job4j.puzzle;
 
 public class Win {
+    public static boolean checkRow(int[][] board, int row) {
+        boolean res = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static boolean checkCol(int[][] board, int col) {
+        boolean res = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] != 1) {
+                res = false;
+                break;
+            }
+        }
+        return res;
+    }
+
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                if (!(checkCol(board, i) || checkRow(board, i))) {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
         return rsl;
     }
 }
